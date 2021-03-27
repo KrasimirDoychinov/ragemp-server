5ef1c709f6f74e36b6a80117c211fc2577207457
Votssy <forcodbo@gmail.com>
2021-03-27 22:56:48 +0200

    Added give weapons command

--- packages/commands/index.js
+++ packages/commands/index.js
@@ -10,3 +10,6 @@ mp.events.addCommand('kill', (player) => {
 mp.events.addCommand('heal', (player) => {
     player.health = 100;
 });
+mp.events.addCommand('weapons', (player, command) => {
+    player.giveWeapon([3220176749, 2210333304, 1593441988, -1045183535, 2144741730, -1063057011, 100416529, 584646201], 1000); // Assault Rifle, Carbine Rifle
+});
