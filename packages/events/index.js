d6e8a577cf2701ef86ec55fb617d7afa5992dde1
NqkuvAccount <spotaflymail@gmail.com>
2021-03-27 13:21:32 -0700

    Fixed syntax error

--- packages/events/index.js
+++ packages/events/index.js
@@ -1,4 +1,4 @@
-﻿mp.events.add('playerDeath', (player) => {]
+﻿mp.events.add('playerDeath', (player) => {
     player.spawn(new mp.Vector3(-425.517, 1123.620, 325.8544));
     player.health = 100;
 });
