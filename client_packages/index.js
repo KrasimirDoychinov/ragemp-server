1f384cc0c20b3a0f9fe55a0696d7a34f1407dac3
NqkuvAccount <spotaflymail@gmail.com>
2021-03-27 08:37:08 -0700

    Added Acc+Char create

--- client_packages/index.js
+++ client_packages/index.js
@@ -1,5 +1,9 @@
+mp.events.add("playerQuit", (player) => {
+    mp.game.graphics.notify(`<C>${player.name}</C> (ID:${player.remoteId}) left the server`);
+});
 require('./login.js');
 
 mp.events.add('playerReady', () => {
     mp.events.call('client:showLoginScreen');
 });
+require('charcreator');
---
+++ packages/index.js
@@ -0,0 +1,13 @@
+function playerQuitHandler(player, exitType, reason) {
+    let str = player.name;
+  
+    if (exitType != "kicked") {
+      str += " quit.";
+    } else {
+      str = ` kicked. Reason: ${reason}.`;
+    }
+  
+    console.log(str);
+  }
+  
+  mp.events.add("playerQuit", playerQuitHandler);
